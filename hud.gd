fdef63b8398be59bce2807db9d7e905d5c72354a
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2025-01-31 10:28:26 +0100

    Initial commit

---
+++ hud.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+signal start_game
+
+func update_score(value):
+	$MarginContainer/Score.text = str(value)
+	
+func update_time(value):
+	$MarginContainer/Time.text = str(value)
+	
+func show_message(text):
+	$message.text = text
+	$message.show()
+	$Timer.start()
+
+func show_game_over():
+	show_message("Game Over")
+	await $Timer.timeout
+	$StartButton.show()
+	$message.text = "Coin Dash!"
+	$message.show()
+
+func _on_timer_timeout() -> void:
+	$message.hide()
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	$message.hide()
+	start_game.emit()
---
+++ main.gd
@@ -0,0 +1,66 @@
+extends Node
+@export var coin_scene : PackedScene
+@export var playtime = 30
+
+var level = 1
+var score = 0
+var time_left = 0
+var screensize = Vector2.ZERO
+var playing = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize;
+	$Player.hide()
+	$HUD.update_score(score)
+	$HUD.update_time(time_left)
+	
+func new_game():
+	playing = true
+	level = 1
+	score = 0
+	time_left = playtime
+	$Player.start()
+	$Player.show()
+	$GameTimer.start()
+	spawn_coins()
+
+func spawn_coins():
+	for i in level + 4:
+		var c = coin_scene.instantiate()
+		add_child(c)
+		c.screensize = screensize
+		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
+		
+
+func _on_game_timer_timeout() -> void:
+	time_left -= 1
+	$HUD.update_time(time_left)
+	if(time_left < 1):
+		game_over()
+
+
+func _on_player_pickup() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+func _on_player_hurt() -> void:
+	game_over()
+
+func _on_hud_start_game() -> void:
+	new_game()
